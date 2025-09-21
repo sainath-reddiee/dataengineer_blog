096474005b2301721882f30bd7f2790a0deccbb3
Sainath Reddy <37265003+sainath-reddiee@users.noreply.github.com>
2025-09-21 16:57:42 +0530

    Update functions.php

--- functions.php
+++ functions.php
@@ -5,9 +5,7 @@
 function handle_cors_requests() {
     $allowed_origins = array(
         'https://app.dataengineerhub.blog',
-        'https://dataengineerhub.blog',
-        'https://localhost:5173',
-        'http://localhost:5173'
+        'https://dataengineerhub.blog'
     );
     
     $origin = isset($_SERVER['HTTP_ORIGIN']) ? $_SERVER['HTTP_ORIGIN'] : '';
